bed4f1e300729affe2c309c466a6a2041daed235
Wes Roach <wesr000@gmail.com>
2022-08-21 13:37:19 -0500

    Running without sleeps.

--- cross-service-communication/main.go
+++ cross-service-communication/main.go
@@ -22,14 +22,15 @@ func main() {
 	go ServiceFold(intoFold)
 
 	// Print all garments in wardrobe
-	// for garmentId := range wardrobe.garment {
-	// 	fmt.Printf("garmentId: %v\n", garmentId)
-	// }
+	for garmentId := range wardrobe.garment {
+		fmt.Printf("garmentId: %v\n", garmentId)
+	}
+	fmt.Printf("%+v, %+v\n", cap(wardrobe.garment), len(wardrobe.garment))
 
 	// Send address of Garments from wardrobe into ServiceWash
 	fmt.Printf("Starting to send Garments into Wash.\n")
 	for garmentId := range wardrobe.garment {
-		time.Sleep(time.Second * 1)
+		// time.Sleep(time.Second * 1)
 		fmt.Printf("%v: ServiceWorker sending %+v into Wash.\n", time.Now(), garmentId)
 		intoWash <- &wardrobe.garment[garmentId]
 	}
--- cross-service-communication/service.go
+++ cross-service-communication/service.go
@@ -9,7 +9,7 @@ func ServiceWash(receiveChannel chan *Garment, sendChannel chan *Garment) {
 	for {
 		garment := <-receiveChannel
 		fmt.Printf("%v: ServiceWash received %+v.\n", time.Now(), garment)
-		time.Sleep(time.Second * 1)
+		// time.Sleep(time.Second * 1)
 		sendChannel <- garment
 		fmt.Printf("%v: ServiceWash passed %+v to Dryer.\n", time.Now(), garment)
 	}
@@ -19,7 +19,7 @@ func ServiceDry(receiveChannel chan *Garment, sendChannel chan *Garment) {
 	for {
 		garment := <-receiveChannel
 		fmt.Printf("%v: ServiceDry received %+v.\n", time.Now(), garment)
-		time.Sleep(time.Second * 2)
+		// time.Sleep(time.Second * 2)
 		sendChannel <- garment
 		fmt.Printf("%v: ServiceDry passed %+v to Fold.\n", time.Now(), garment)
 	}
@@ -29,6 +29,6 @@ func ServiceFold(receiveChannel chan *Garment) {
 	for {
 		garment := <-receiveChannel
 		fmt.Printf("%v: ServiceFold received %+v.\n", time.Now(), garment)
-		time.Sleep(time.Second * 1)
+		// time.Sleep(time.Second * 1)
 	}
 }
